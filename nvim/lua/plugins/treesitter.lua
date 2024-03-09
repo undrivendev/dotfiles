5d8124e8e40141641eedc1d485294dcf66a9e73b
Luca Dalla Valle <luca.dallavalle@outlook.com>
2024-03-09 18:43:52 +0100

    feat: add treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+M = {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local ts_config = require("nvim-treesitter.configs")
+    ts_config.setup({
+      ensure_installed = { "lua" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
+
+return M
